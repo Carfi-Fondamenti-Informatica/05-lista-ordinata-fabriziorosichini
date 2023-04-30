c705f0c8956af38f370dcd2e96d0b1f3b88502f2
fabriziorosichini <127482458+fabriziorosichini@users.noreply.github.com>
2023-04-30 23:09:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,20 +8,16 @@ int main() {
     int a = 0;
     cin >> a;
 
-    /* copio la lista in ingresso*/
     int copy[10];
     for (int i = 0; i < 10; i++) {
         copy[i] = lista[i];
     }
     int i = 0;
 
-    /* gli i-esimi valori < di a rimangono nella loro posizione*/
     while (lista[i] <= a) {
         lista[i] = copy[i];
         i++;
     }
-    /* se l'i-esimo valore della lista è uguale ad a, inserisco a mentre quelli successivi prendono il
-     * valore dell'indice precedente dalla lista copy*/
     if (lista[i] == a) {
         while (lista[i] == a) {
             lista[i] = a;
@@ -32,8 +28,7 @@ int main() {
             lista[i] = copy[i - 1];
             i++;
         }
-        /* se invece a>lista [i] ma a non è nella lista, l'i-esimo numero assume il valore di a,
-         * quelli successivi prendono il valore dell'indice precedente dalla lista copy*/
+       
     } else {
         lista[i] = a;
         i++;
